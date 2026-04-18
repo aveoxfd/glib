df2e240e84e83f1e08a496899008334459c4f58b
.ffd <krekerbruh@hotmail.com>
2026-04-18 16:17:33 +0300

    Add copy command to build script for library header inclusion

--- buildlib.bat
+++ buildlib.bat
@@ -3,3 +3,5 @@ g++ -c glib/src/Widget.cpp -o glib/bin/Widget.o -I include/glib
 g++ -c glib/src/Event.cpp -o glib/bin/Event.o -I include/glib
 
 ar rcs glib/lib/glib.lib glib/bin/ClassWindow.o glib/bin/Event.o glib/bin/Widget.o
+
+copy glib\lib\glib.lib include\glib\
