94ca7e66f0da024e0140dc2ab4a9b2d0d1b714fa
.ffd <krekerbruh@hotmail.com>
2026-04-21 18:14:22 +0300

    Add build script for compiling and archiving glib library

---
+++ libdebug.bat
@@ -0,0 +1,7 @@
+g++ -c glib/src/ClassWindow.cpp -o glib/bin/ClassWindow.o -I include/glib -DDEBUG
+g++ -c glib/src/Widget.cpp -o glib/bin/Widget.o -I include/glib -DDEBUG
+g++ -c glib/src/Event.cpp -o glib/bin/Event.o -I include/glib -DDEBUG
+
+ar rcs glib/lib/glib.lib glib/bin/ClassWindow.o glib/bin/Event.o glib/bin/Widget.o
+
+copy glib\lib\glib.lib include\glib\
