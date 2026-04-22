e3ea60996f6dfb212535b527a970845ed74fdff0
.ffd <krekerbruh@hotmail.com>
2026-04-22 06:54:18 +0300

    Enhance Event class to support user data in event handling

--- Include/glib/Event.h
+++ Include/glib/Event.h
@@ -5,13 +5,15 @@ class Widget;
 
 class Event{
     private:
-    typedef void(*EventFunction)(Widget* sender);
+    typedef void(*EventFunction)(Widget* sender, void *data);
     EventFunction event_function;
     Widget *connection;
+    void *user_data;
     public:
-    Event(EventFunction function = nullptr, Widget *connected = nullptr);
+    Event(EventFunction function, void* data = nullptr, Widget *connected = nullptr);
     Event(EventFunction function);
     void activate(Widget *sender = nullptr);
+    void* get_data() const { return user_data; }
 };
 
 #endif
--- glib/src/Event.cpp
+++ glib/src/Event.cpp
@@ -1,12 +1,15 @@
 #include "Event.h"
 #include "Widget.h"
 
-Event::Event(EventFunction function, Widget *connected):event_function(function), connection(connected){
+Event::Event(EventFunction function, void* data, Widget *connected):
+event_function(function),
+user_data(data),
+connection(connected){
 }
 Event::Event(EventFunction function):event_function(function){}
 
 void Event::activate(Widget *sender){
     if (!event_function)return;
-    event_function(sender ? sender : connection);
+    event_function(sender ? sender : connection, user_data);
     return;
 }
