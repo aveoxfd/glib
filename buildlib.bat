bb06e0c605f9a7df2fd501e9794de5d53b44b340
.ffd <krekerbruh@hotmail.com>
2026-04-26 17:02:16 +0300

    Add pause command to build script for better user interaction

--- buildlib.bat
+++ buildlib.bat
@@ -5,3 +5,5 @@ g++ -c glib/src/Event.cpp -o glib/bin/Event.o -I include/glib
 ar rcs glib/lib/glib.lib glib/bin/ClassWindow.o glib/bin/Event.o glib/bin/Widget.o
 
 copy glib\lib\glib.lib include\glib\
+
+pause
