e52d5f3071f43137a208e1eb0618473f5190f830
.ffd <krekerbruh@hotmail.com>
2026-05-08 08:27:07 +0300

    Refactor timer documentation in Timer.cpp for clarity and completeness

--- glib/src/Timer.cpp
+++ glib/src/Timer.cpp
@@ -1,11 +1,20 @@
 #include <Windows.h>
-
 /*
-static void CALLBACK GlobalTimerProc(HWND hWnd, ){
+https://learn.microsoft.com/en-us/windows/win32/api/winuser/nc-winuser-timerproc
 
-}
 
-UINT_PTR timerId = SetTimer(NULL, 0, 1000, NULL);
+If the function succeeds and the hWnd parameter is NULL, the return value is an integer identifying the new timer. 
+An application can pass this value to the KillTimer function to destroy the timer.
 
-TIMERPROC x;
+If the function succeeds and the hWnd parameter is not NULL, then the return value is a nonzero integer. 
+An application can pass the value of the nIDEvent parameter to the KillTimer function to destroy the timer.
+
+If the function fails to create a timer, the return value is zero. To get extended error information, call GetLastError.
 */
+
+
+#include "../../include/glib/ClassWindow.h"
+
+static void CALLBACK GlobalTimerProc(HWND hWnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime){
+    //...
+}
